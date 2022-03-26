50886878639e5bb11f002bfcc8095e2fedd79be0
Ekanshh Praveen <edpravee@uwaterloo.ca>
2022-03-26 01:10:19 -0400

    initial transaction code

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { MnemonicKey, MsgSend, LCDClient } from "@terra-money/terra.js";
+
+const terra = new LCDClient({
+    URL: 'https://localhost:1317',
+    chainID: 'localterra',
+});
+
+const mk = new MnemonicKey({
+    mnemonic: "<Enter 24 word mnemonic>",
+});
+
+//creating a wallet
+const wallet = terra.wallet(mk);
+
+//transaction
+
+//creating a message
+const send = new MsgSend(wallet.key.accAddress,
+    "some addresss",
+    { uluna: 1000 });
+
+//creating and signing a transaction
+const tx = await wallet.createAndSignTx({
+    msgs: [send],
+    memo: "Hello"
+});
+
+const txResult = await terra.tx.broadcast(tx);
+
